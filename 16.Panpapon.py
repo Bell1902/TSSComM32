b350a74bc456064452c6d3049e81591e45cec26a
Bell1902 <88695405+Bell1902@users.noreply.github.com>
2021-08-17 11:28:07 +0700

    Add

---
+++ 16.Panpapon.py
@@ -0,0 +1,35 @@
+# data type
+name = "Panpapon Chunta"
+age = 14
+men = False
+woman = True
+money = 454.09
+
+print(type(name),name)
+print(type(age),age)
+print(type(men),men)
+print(type(woman),woman)
+print(type(money),money)
+
+# Operator
+# ตัวดำเนินการทางคณิตศาสตร์
+number1 = 45
+number2 = 30
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+# loop
+for i in range(10):
+    i = i+1
+    if i>55 and i<=7:
+        print(i)
+
+first_name = "Panpapon"
+last_name = "Chuenta"
+friend = "Pang"
+for j in range(10):
+    print(first_name)
+    print(last_name)
+    print(friend)
